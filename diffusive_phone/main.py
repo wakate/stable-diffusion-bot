670d28b35c771fbadfd84e941217b0760dd5e920
mt_caret <mtakeda.enigsol@gmail.com>
2022-09-04 05:17:11 +0000

    add inference

--- diffusive_phone/main.py
+++ diffusive_phone/main.py
@@ -1,14 +1,30 @@
 import os
+import time
+from concurrent.futures import ProcessPoolExecutor
+from io import BytesIO
+import asyncio
+
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
+from starlette.responses import StreamingResponse
 
 from diffusers import StableDiffusionPipeline
+import torch
 
 diffusion_token = os.environ['DIFFUSION_TOKEN']
+
+load_start = time.time()
 print('Loading model...')
-pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=diffusion_token)
-print('Loading model done.')
+pipe = StableDiffusionPipeline.from_pretrained(
+    "CompVis/stable-diffusion-v1-4",
+    revision='fp16',
+    torch_dtype=torch.float16,
+    use_auth_token=diffusion_token
+)
+pipe.to('cuda')
+load_end = time.time()
+print(f'Loading model done in {load_end - load_start} seconds')
 
 app = FastAPI()
 
@@ -19,8 +35,27 @@ def read_root():
 class Query(BaseModel):
     prompt: str
 
+def generate_image(prompt):
+    with torch.autocast('cuda'):
+        return pipe(prompt)['sample'][0]
+
 @app.post('/query')
-async def run_inference(query: Query):
-    with ProcessPoolExecutor(max_tasks=1) as executor:
-        result = asyncio.get_running_loop().run_in_executor(exector, pipe, query.prompt)
-    return result
+def run_inference(query: Query):
+    with torch.autocast('cuda'):
+        image = pipe(query.prompt)['sample'][0]
+
+    buffer = BytesIO()
+    image.save(buffer, format='PNG')
+    buffer.seek(0)
+    return StreamingResponse(buffer, media_type='image/png')
+
+# Below unfortunately doesn't work; it tries to load the model multiple times, running out of memory... :(
+#@app.post('/query')
+#async def run_inference(query: Query):
+#    with ProcessPoolExecutor() as executor:
+#        image = await asyncio.get_running_loop().run_in_executor(executor, generate_image, query.prompt)
+#
+#    buffer = BytesIO()
+#    image.save(buffer, format='PNG')
+#    buffer.seek(0)
+#    return StreamingResponse(buffer, media_type='image/png')
